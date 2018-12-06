37553305cf7a2277232c9ebd8903e6649ae660d2
sczempiel <sczempiel@users.noreply.github.com>
2018-12-06 07:28:43 +0100

    day6

--- src/day2/Day2Task1Main.java
+++ src/day2/Day2Task1Main.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import util.AdventUtils;
+import util.Touple;
 
 public class Day2Task1Main {
 
--- src/util/Touple.java
+++ src/util/Touple.java
@@ -1,4 +1,4 @@
-package day2;
+package util;
 
 public class Touple<R, L> {
 	private R right;
