a68a5933b7c82519d94435b91f8f13cfec91c322
sczempiel <sczempiel@users.noreply.github.com>
2018-12-14 13:42:33 +0100

    day13

---
+++ src/day14/Day13Task2Main.java
@@ -0,0 +1,21 @@
+package day15;
+
+import java.io.IOException;
+import java.util.List;
+
+import util.AdventUtils;
+
+public class Day13Task2Main {
+
+	public static void main(String[] args) {
+
+		try {
+			List<String> input = AdventUtils.getStringInput(13);
+
+			AdventUtils.publishResult(13, 2, "");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
